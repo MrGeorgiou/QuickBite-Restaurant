0b7457b5204d190a518b5cf499c8fcdf965f5a08
Michael Georgiou <mikg25@gmail.com>
2023-07-16 19:03:12 +0100

    opening hours grid started

--- app.js
+++ app.js
@@ -39,3 +39,17 @@ function parallaxEffect() {
 }
 
 window.addEventListener("scroll", parallaxEffect);
+
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
