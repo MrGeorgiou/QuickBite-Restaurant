51d1b54e75255d1045c9e2367d5934ba9c1cf23a
Michael Georgiou <mikg25@gmail.com>
2023-07-22 01:10:02 +0100

    Parallax finishing touch

--- app.js
+++ app.js
@@ -28,13 +28,15 @@ navMenuExitButton.addEventListener("click", NavMenuOff);
 
 
 //PARALLAX EFFECT
+const foreground = document.getElementById("foreground-2")
 const middle = document.getElementById("middle");
 const text = document.getElementById("text");
-const scrollPrompt = document.getElementById("scroll-prompt")
+const scrollPrompt = document.getElementById("scroll-prompt");
 
 function parallaxEffect() {
     let value = window.scrollY;
     scrollPrompt.textContent ="";
+    foreground.style.top = value * 0.3 + "px";
     middle.style.top = value * 0.4 + "px";
     text.style.top = value * 1.2 + "px";
     text.style.opacity = 1 - (value / 150);
