8c1b348bec076d8cfca52be55127d882fc32a985
Michael Georgiou <mikg25@gmail.com>
2023-08-03 13:40:18 +0100

    about page navbar problem

--- scripts/about.js
+++ scripts/about.js
@@ -6,12 +6,31 @@ subMenuAboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
 function navBackgroundFadein() {
     const navAboutPageOpacityMax = "rgba(241, 158, 57, 0.9)";
     const value = window.scrollY;
-    const rgbaOpacityValue = value * 0.001;
+    const rgbaOpacityValue = value * 0.002;
 
     if (navAboutPage.style.backgroundColor !== navAboutPageOpacityMax  || rgbaOpacityValue < navAboutPageOpacityMax) {
         subMenuAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
         navAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
     }
 }
+
+// function navBackgroundFadein() {
+//     const navAboutPageOpacityMax = "rgba(241, 158, 57, 0.9)";
+//     const value = window.scrollY;
+//     const rgbaOpacityValue = value * 0.002;
+//     const navBackgroundColor = navAboutPage.style.backgroundColor;
+//     let navBackgroundOpacity = navBackgroundColor.substr(19);
+//     navBackgroundOpacity = navBackgroundOpacity.slice(0, -1);
+
+//     // if (navAboutPage.style.backgroundColor !== navAboutPageOpacityMax  && 
+//     if (navBackgroundOpacity <= 0.9) {
+//         subMenuAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
+//         navAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
+//     } 
+
+//     console.log(navBackgroundColor);
+//     console.log(navBackgroundOpacity);
+// }
+
 
 window.addEventListener("scroll", navBackgroundFadein);
