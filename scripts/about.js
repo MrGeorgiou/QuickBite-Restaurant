4241579e9aca67f9c14a16355b500c2210382f81
Michael Georgiou <mikg25@gmail.com>
2023-08-06 18:38:59 +0100

    Nav and parallax

--- scripts/about.js
+++ scripts/about.js
@@ -1,8 +1,12 @@
 const navAboutPage = document.getElementById("about-pg-nav");
 const subMenuAboutPage = document.getElementById("about-sub-menu");
 
-subMenuAboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+subMenuAboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
  
+if (window.scrollY) {
+    navBackgroundFadein();
+}
+
 function navBackgroundFadein() {
     const navAboutPageOpacityMax = "rgba(241, 158, 57, 0.9)";
     const value = window.scrollY;
@@ -12,25 +16,11 @@ function navBackgroundFadein() {
         subMenuAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
         navAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
     }
+
+    if (window.scrollY < 10) {
+        subMenuAboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+    }
 }
-
-// function navBackgroundFadein() {
-//     const navAboutPageOpacityMax = "rgba(241, 158, 57, 0.9)";
-//     const value = window.scrollY;
-//     const rgbaOpacityValue = value * 0.002;
-//     const navBackgroundColor = navAboutPage.style.backgroundColor;
-//     let navBackgroundOpacity = navBackgroundColor.substr(19);
-//     navBackgroundOpacity = navBackgroundOpacity.slice(0, -1);
-
-//     // if (navAboutPage.style.backgroundColor !== navAboutPageOpacityMax  && 
-//     if (navBackgroundOpacity <= 0.9) {
-//         subMenuAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
-//         navAboutPage.style.backgroundColor = "rgba(241, 158, 57, " + rgbaOpacityValue + ")";
-//     } 
-
-//     console.log(navBackgroundColor);
-//     console.log(navBackgroundOpacity);
-// }
 
 
 window.addEventListener("scroll", navBackgroundFadein);
--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,5 +1,6 @@
 //PARALLAX EFFECT
-const foreground = document.getElementById("foreground-2")
+const background = document.getElementById("background");
+const foreground = document.getElementById("foreground-2");
 const middle = document.getElementById("middle");
 const text = document.getElementById("text");
 const scrollPrompt = document.getElementById("scroll-prompt");
@@ -7,8 +8,10 @@ const scrollPrompt = document.getElementById("scroll-prompt");
 function parallaxEffect() {
     let value = window.scrollY;
     scrollPrompt.textContent ="";
+    background.style.top = value * 0.4 + "px";
     foreground.style.top = value * 0.3 + "px";
     middle.style.top = value * 0.4 + "px";
+
     text.style.top = value * 1.2 + "px";
     text.style.opacity = 1 - (value / 150);
 }
