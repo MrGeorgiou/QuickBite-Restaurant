9d22be36dc548aa130cb8d4c81d853c5413e2829
Michael Georgiou <mikg25@gmail.com>
2023-08-12 19:23:33 +0100

    removed js parallax effect from about-us pg

--- scripts/about.js
+++ scripts/about.js
@@ -1,26 +1,24 @@
-// const navAboutPage = document.getElementById("about-pg-nav");
-// const subMenuAboutPage = document.getElementById("about-sub-menu");
-
-// subMenuAboutPage.style.backgroundColor = "transparent";
+const navAboutPage = document.getElementById("about-pg-nav");
+const subMenuAboutPage = document.getElementById("about-sub-menu");
  
-// if (window.scrollY) {
-//     navBackgroundFadein();
-// }
+if (window.scrollY) {
+    navBackgroundFadein();
+}
 
-// function navBackgroundFadein() {
-// console.log(window.scrollY);
-//     if (window.scrollY < 1) {
-//         navAboutPage.style.backgroundColor = "transparent"
-//         subMenuAboutPage.style.backgroundColor = "transparent";
-//     } else {
-//         navAboutPage.style.backgroundColor = "rgba(241, 158, 57, 1)";
-//         subMenuAboutPage.style.backgroundColor = "rgba(241, 158, 57, 1)";
-//     }
-// }
+function navBackgroundFadein() {
+console.log(window.scrollY);
+    if (window.scrollY < 1) {
+        navAboutPage.style.backgroundColor = "transparent"
+        subMenuAboutPage.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+    } else {
+        navAboutPage.style.backgroundColor = "rgba(241, 158, 57, 1)";
+        subMenuAboutPage.style.backgroundColor = "rgba(241, 158, 57, 1)";
+    }
+}
 
-// window.addEventListener("scroll", navBackgroundFadein);
+window.addEventListener("scroll", navBackgroundFadein);
 
 
 
-const greetingText = document.querySelector("div#text h1")
+const greetingText = document.querySelector("div#text")
 window.addEventListener("scroll", () => { highlightText(greetingText) });
