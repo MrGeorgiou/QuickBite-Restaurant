9bea4ad9807325da13ca117ce3fb71d8b87200f0
Michael Georgiou <mikg25@gmail.com>
2023-08-12 11:43:36 +0100

    Menu background change

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -12,7 +12,7 @@ function parallaxEffect() {
     foreground.style.top = value * 0.3 + "px";
     middle.style.top = value * 0.4 + "px";
 
-    text.style.top = value * 1.2 + "px";
+    // text.style.top = value * 1 + "px";
     text.style.opacity = 1 - (value / 150);
 }
 
