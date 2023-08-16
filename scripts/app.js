4898dd157d431ade933e005884193c60e80ba577
Michael Georgiou <mikg25@gmail.com>
2023-08-16 21:55:00 +0100

    scroll prompt added

--- scripts/app.js
+++ scripts/app.js
@@ -125,3 +125,24 @@ window.addEventListener("scroll", () => { highlightText(welcomeText) });
 // }
 
 // window.addEventListener("scroll", onScroll)
+
+//SCROLL BUTTON PROMPT
+const downButton = document.getElementById("down-button")
+const scrollPrompt = document.getElementById("scroll-prompt")
+const downButtonContainer = document.getElementById("down-button-container")
+
+function displayPrompt() {
+    downButton.style.display = "none";
+    scrollPrompt.style.display = "block";
+}
+
+function resetButton() {
+    scrollPrompt.style.display = "none";
+    downButton.style.display = "block";
+}
+
+
+console.log(downButtonContainer)
+downButtonContainer.addEventListener("mouseenter", displayPrompt)
+downButtonContainer.addEventListener("mouseleave", resetButton)
+
