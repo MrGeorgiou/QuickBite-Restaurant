070c1bbb9ac41d81a9007f8d3c1b6c52b0062bfc
Michael Georgiou <mikg25@gmail.com>
2023-08-26 22:22:24 +0100

    continue button changes

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,14 @@ window.addEventListener("scroll", () => { highlightText(welcomeText) });
 // SCROLL BUTTON PROMPT
 const downButton = document.getElementById("down-button")
 const scrollPrompt = document.getElementById("scroll-prompt")
-const downButtonContainer = document.querySelector(".down-button-container")
+const downButtonContainer = document.querySelector(".button-container.down")
 
 downButtonContainer.addEventListener("mouseenter", () => { displayPrompt(downButton, scrollPrompt) });
 downButtonContainer.addEventListener("mouseleave", () => { resetButton(scrollPrompt, downButton) });
+
+const sideButton = document.getElementById("side-button")
+const continuePrompt = document.getElementById("continue-prompt")
+const sideButtonContainer = document.querySelector(".button-container.side")
+
+sideButtonContainer.addEventListener("mouseenter", () => { displayPrompt(sideButton, continuePrompt) });
+sideButtonContainer.addEventListener("mouseleave", () => { resetButton(continuePrompt, sideButton) });
